699301903e26979527250dcd50994d79cbfd00d4
AJeich <adameich@creighton.edu>
2019-10-09 16:35:59 -0500

    Midterm
    midterm

--- forms/animalXP/animalXP.js
+++ forms/animalXP/animalXP.js
@@ -2,12 +2,16 @@
 
 let animals = ['dog', 'cat', 'horse', 'mouse', 'pig', 'cow', 'ferret', 'lizard', 'frog']
 
-let userAnimal = prompt("enter the name of an animal")
+for (i = 0; i < 2; i++) {
+
+  let userAnimal = prompt("enter the name of an animal")
 
   userAnimal = userAnimal.toLowerCase()
 
   animals.push(userAnimal)
 
-let animalAnswer = alert(`The last animal is ${userAnimal}`)
+  let animalAnswer = alert(`The last animal is ${userAnimal}`)
+
+}
 
 */
--- forms/incomeBracket/incomeBracket.js
+++ forms/incomeBracket/incomeBracket.js
@@ -2,11 +2,11 @@
 btnSubmit.onclick=function(){
   let income = Number(inptIncome.value)
   if (income < 30000) {
-    lblAnswer.text = (`With your income of $${income}, you are in a tax bracket of 8%`)
+      lblAnswer.text = (`With your income of $${income}, you are in a tax bracket of 8%`)
   } else if (income > 30000 && income < 99999) {
-    lblAnswer.text = (`With your income of $${income}, you are in a tax bracket of 15%`)
+      lblAnswer.text = (`With your income of $${income}, you are in a tax bracket of 15%`)
   } else {
-    lblAnswer.text = (`With your income of $${income}, you are in a tax bracket of 25%`)
+      lblAnswer.text = (`With your income of $${income}, you are in a tax bracket of 25%`)
   }
   
 
